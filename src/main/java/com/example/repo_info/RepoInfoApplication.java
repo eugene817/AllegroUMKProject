b5c24948847bf0783f6bb90d347ece7110358945
Yauheni <=>
2025-06-23 00:50:56 +0200

    initing

---
+++ src/main/java/com/example/repo_info/RepoInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.repo_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/repo_info/RepoInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repo_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
